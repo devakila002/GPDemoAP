c3119d5225942d91be613b2bb2895c6cd7744ae4
devakila <dev.akila001@gmail.com>
2023-08-28 21:53:49 +0530

    Added Payment Test and Payment.cs

---
+++ GP.Tests/PaymentTest.cs
@@ -0,0 +1,18 @@
+namespace GP.Tests;
+
+public class PaymentTest
+{
+    [Fact]
+    public void CheckIfPaymenthasPaidTest()
+    {
+        //Arrange
+      
+        //Act
+        //Assert
+
+    }
+
+    
+}
+
+    
