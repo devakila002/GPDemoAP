a391068d9a6a4023c70c4ed49d487b3be732d249
devakila <dev.akila001@gmail.com>
2023-08-28 21:35:51 +0530

    Created Classlib Project

---
+++ GP.Tests/ViewTest.cs
@@ -0,0 +1,17 @@
+namespace GP.Tests;
+
+public class ViewTest
+{
+    [Fact]
+    public void CheckIfViewWorkingCorrectTest()
+    {
+        //Arrange
+      
+        //Act
+        //Assert
+
+    }
+    
+}
+
+    
